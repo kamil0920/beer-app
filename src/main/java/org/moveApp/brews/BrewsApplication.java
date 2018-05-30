d0b5fb5bf5042217f61c19495f96a00dd71ac625
terrato92 <kamilmituraa@gmail.com>
2018-05-30 17:43:10 +0200

    first commit

---
+++ src/main/java/org/moveApp/brews/BrewsApplication.java
@@ -0,0 +1,12 @@
+package org.moveApp.brews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewsApplication.class, args);
+	}
+}
---
+++ src/test/java/org/moveApp/brews/BrewsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.moveApp.brews;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
