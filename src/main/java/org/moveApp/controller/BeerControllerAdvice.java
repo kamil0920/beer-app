90685db41fc90fa15e84e47597184903d7c14e72
terrato92 <kamilmituraa@gmail.com>
2018-06-02 18:57:58 +0200

    controller advice commit

--- src/main/java/org/moveApp/controller/BeerControllerAdvice.java
+++ src/main/java/org/moveApp/controller/BeerControllerAdvice.java
@@ -1,6 +1,6 @@
 package org.moveApp.controller;
 
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.moveApp.exception.ResourceNotFound;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
-public class BeerControllerAdvice {
+public class BeerControllerAdvice extends Throwable {
 
-    @ExceptionHandler({ResourceNotFoundException.class})
+    @ExceptionHandler({ResourceNotFound.class})
     public ResponseEntity<Object> handleNotFoundException(Exception exception, WebRequest request) {
 
         return new ResponseEntity<Object>("Resource Not Found", new HttpHeaders(), HttpStatus.NOT_FOUND);
