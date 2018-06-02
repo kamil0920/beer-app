ab831bb224cdfa1593d1dd8f079ed74ab8ed894b
terrato92 <kamilmituraa@gmail.com>
2018-06-02 02:43:18 +0200

    controller commit

---
+++ src/main/java/org/moveApp/controller/BeerController.java
@@ -0,0 +1,37 @@
+package org.moveApp.controller;
+
+
+import org.moveApp.domain.DataBeer;
+import org.moveApp.service.BeerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BeerController {
+
+    @Autowired
+    private BeerService beerService;
+
+
+    @GetMapping
+    public ResponseEntity<List<DataBeer>> getBeers(){
+
+        List<DataBeer> beerData = beerService.getAllBeers();
+
+        return new ResponseEntity<List<DataBeer>>(beerData, HttpStatus.OK);
+    }
+
+    @GetMapping("/foodpairings/search/{phrase}")
+    public ResponseEntity<List<DataBeer>> getBeerByPhrease(@PathVariable String phrase){
+
+        List<DataBeer> beerData = beerService.findBeerByPhrase(phrase);
+
+        return new ResponseEntity<List<DataBeer>>(beerData, HttpStatus.OK);
+    }
+}
