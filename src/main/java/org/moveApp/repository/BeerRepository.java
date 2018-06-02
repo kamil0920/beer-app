a2c8037c51586fdf1df090b53ad29ed65a24bcc7
terrato92 <kamilmituraa@gmail.com>
2018-06-02 13:49:02 +0200

    repo commit

--- src/main/java/org/moveApp/repository/BeerRepository.java
+++ src/main/java/org/moveApp/repository/BeerRepository.java
@@ -5,7 +5,10 @@ import org.moveApp.domain.DataBeer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BeerRepository extends CrudRepository<DataBeer, Long> {
 
+    Optional<DataBeer> findByPunkapiId(Integer id);
 }
