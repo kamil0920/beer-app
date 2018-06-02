ea89fe4c942b2df41956939ba5ea78ec6faf5e3e
terrato92 <kamilmituraa@gmail.com>
2018-06-02 02:43:42 +0200

    domain commit

---
+++ src/main/java/org/moveApp/domain/DataBeer.java
@@ -0,0 +1,40 @@
+package org.moveApp.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+@Table(name = "beers")
+@Data
+public class DataBeer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer punkapiId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String tagline;
+
+    @Column
+    private String firstBrewed;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column
+    private String imageUrl;
+
+    @Column
+    private Float ibu;
+
+    @Column(name = "food_pairing")
+    @ElementCollection(targetClass=String.class)
+    private List<String> foodPairing = new LinkedList<>();
+
+}
