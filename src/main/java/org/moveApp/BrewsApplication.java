5836fd8e606b67deeac0c37a9fa6bde0509cd247
terrato92 <kamilmituraa@gmail.com>
2018-06-05 16:05:24 +0200

    start data commit

--- src/main/java/org/moveApp/BrewsApplication.java
+++ src/main/java/org/moveApp/BrewsApplication.java
@@ -1,14 +1,26 @@
 package org.moveApp;
 
+import org.moveApp.service.impl.IBeerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
-public class BrewsApplication {
+public class BrewsApplication  extends SpringBootServletInitializer {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		SpringApplication.run(BrewsApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(BrewsApplication.class, args);
+
+		context.getBean(IBeerService.class).getAllBeers();
 	}
+
+
+
+	// call method when spring boot run
+
+	// spring cron http://www.baeldung.com/cron-expressions
 }
