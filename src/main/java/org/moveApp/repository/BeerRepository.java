21a00fa12ea3ceaa3c02f13fd9c2c8f5e7f9f403
terrato92 <kamilmituraa@gmail.com>
2018-06-05 15:58:13 +0200

    service commit

--- src/main/java/org/moveApp/repository/BeerRepository.java
+++ src/main/java/org/moveApp/repository/BeerRepository.java
@@ -2,13 +2,19 @@ package org.moveApp.repository;
 
 
 import org.moveApp.domain.DataBeer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BeerRepository extends CrudRepository<DataBeer, Long> {
 
     Optional<DataBeer> findByPunkapiId(Integer id);
+
+    @Query("select b from DataBeer b join b.foodPairing fp where fp like %:phrase%")
+    Optional<List<DataBeer>> findByFoodPairingPhrase(@Param("phrase") String phrase);
 }
