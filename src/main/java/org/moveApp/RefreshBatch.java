5ea050cc2728bda89d19ad1b655ef8b05f3a2612
terrato92 <kamilmituraa@gmail.com>
2018-06-05 17:03:15 +0200

    batch commit

--- src/main/java/org/moveApp/RefreshBatch.java
+++ src/main/java/org/moveApp/RefreshBatch.java
@@ -2,7 +2,6 @@ package org.moveApp;
 
 import lombok.extern.slf4j.Slf4j;
 import org.moveApp.domain.DataBeer;
-import org.moveApp.repository.BeerRepository;
 import org.moveApp.service.BeerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,16 +16,17 @@ public class RefreshBatch {
     @Autowired
     private BeerService beerService;
 
-    @Autowired
-    private BeerRepository beerRepository;
+    public RefreshBatch(BeerService beerService) {
+        this.beerService = beerService;
+    }
 
     @Scheduled(
-            cron = "0 0 * * * *")
+            cron = "0,15 * * * * *"
+//                    "0 0 * * * *"
+    )
     public void cronJob(){
 
         List<DataBeer> list = beerService.getAllBeers();
-
-        beerRepository.saveAll(list);
 
         log.info("CRON JOB");
     }
