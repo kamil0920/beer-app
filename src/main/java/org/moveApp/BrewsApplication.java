1c6dde7fc0897c75ff698d72a8c2fae99c8e815f
terrato92 <kamilmituraa@gmail.com>
2018-06-06 19:48:18 +0200

    commit

--- src/main/java/org/moveApp/BrewsApplication.java
+++ src/main/java/org/moveApp/BrewsApplication.java
@@ -2,6 +2,7 @@ package org.moveApp;
 
 import org.moveApp.service.impl.IBeerService;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableCaching
 @EnableScheduling
+@EnableAutoConfiguration
 public class BrewsApplication extends SpringBootServletInitializer {
 
 
